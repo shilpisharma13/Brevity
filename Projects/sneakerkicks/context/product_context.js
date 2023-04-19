6a4d5b47e96721964fd6470175660e9ce49135ee
shilpisharma13 <shilpi13@gmail.com>
2023-04-19 11:55:49 +1000

    new

--- Projects/sneakerkicks/context/product_context.js
+++ Projects/sneakerkicks/context/product_context.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useReducer } from 'react'
 import axios from 'axios'
 // import { options } from '@/utils/constants'
-import { sneakers } from '@/utils/sneakers'
+// import { sneakers } from '@/utils/sneakers'
 import reducer from '../reducers/products_reducer'
 import {
   GET_PRODUCTS_BEGIN,
@@ -22,9 +22,13 @@ export const ProductsProvider = ({ children }) => {
   const fetchProducts = async () => {
     try {
       dispatch({ type: GET_PRODUCTS_BEGIN })
-    //   const { data } = await axios.request(options)
+      // const { data } = await axios.request(options)
     //  const sneakers = data.results
-     
+    
+    const response = await fetch('http://localhost:4000/sneakers')
+   
+     const sneakers = await response.json()
+   
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: sneakers })
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR })
--- Projects/sneakerkicks/utils/constants.js
+++ Projects/sneakerkicks/utils/constants.js
@@ -1,12 +1,4 @@
-export const options = {
-  method: 'GET',
-  url: 'https://the-sneaker-database.p.rapidapi.com/sneakers',
-  params: { limit: '100' },
-  headers: {
-    'X-RapidAPI-Key': 'bafe7a4168msh1a313913b97ab2ep1770b4jsn4c2fae9deacc',
-    'X-RapidAPI-Host': 'the-sneaker-database.p.rapidapi.com',
-  },
-}
+
 
 export const links = [
   {
